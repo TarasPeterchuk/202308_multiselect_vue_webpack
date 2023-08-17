3f7464f32f766f0b1df9f3a4122ced4a3c88cb4b
apache0505 <jiayulutsk@ukr.net>
2023-08-17 16:03:24 +0300

    assafe

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const config = {
   entry: './src/main.js',
@@ -23,7 +24,18 @@ const config = {
   resolve: {
     extensions: ['.js', '.vue']
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyWebpackPlugin({
+      // Add the CopyWebpackPlugin configuration
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public', 'index.html'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    })
+  ]
 }
 
 module.exports = config
